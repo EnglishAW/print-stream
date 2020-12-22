0edfaddf694333ef57d4215de2b487928c954396
Andrew English <english.w.andrew@gmail.com>
2020-12-22 11:08:28 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>Print Stream</h1>
+        <img src="http://99.132.123.210:5001/?action=stream" alt="stream" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
