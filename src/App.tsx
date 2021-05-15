29518e72171f5c9d45d39f0589414aa1c6af50a9
Andrew <english.w.andrew@gmail.com>
2021-05-14 23:44:49 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <div className="App-header">
         <h1>Print Stream</h1>
-        <img src="http://99.132.123.210:5001/html/cam_pic_new.php" alt="stream" />
+        <!-- <img src="http://99.132.123.210:5001/html/cam_pic_new.php" alt="stream" />-->
+        <iframe style="height: 600px; width: 400px; border: none; overflow: none;" src="http://99.132.123.210:5001/html/cam_pic_new.php" title="Stream"></iframe> 
       </div>
     </div>
   );
