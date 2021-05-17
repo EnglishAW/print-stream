75bc7a23a6280fe7731d8b3dbb963ce1375e8e41
Andrew <english.w.andrew@gmail.com>
2021-05-17 12:41:19 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <div className="App-header">
         <h1>Canela's Stream</h1>
-        <!-- <img src="http://99.132.123.210:5001/html/cam_pic_new.php" alt="stream" />-->
-        <iframe style="height: 600px; width: 400px; border: none; overflow: none;" src="http://99.132.123.210:5001/html/cam_pic_new.php" title="Stream"></iframe> 
+        <img src="http://99.132.123.210:5001/html/cam_pic_new.php" alt="stream closed" /> 
       </div>
     </div>
   );
